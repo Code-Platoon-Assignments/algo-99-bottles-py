2c14fd858e19a9088e4f5795b746ec92cdd7c340
fravila08 <fr4v1l4@gmail.com>
2023-08-14 14:53:13 -0700

    done

--- bottles.py
+++ bottles.py
@@ -1,5 +1,14 @@
 def bottle_song():
-	# write your code here!
-	pass
+	bottles = 99
+	while bottles > 2:
+		print(f"""{bottles} bottles of beer on the wall, {bottles} bottles of beer.
+Take one down and pass it around, {bottles - 1} bottles of beer on the wall.""")
+		bottles -= 1
+	print(f"""2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.""")
 
 bottle_song()
